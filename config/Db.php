06bccea93c045ee51558cb50a7c4a061f9137466
saidouux <oumakhlouf.said@gmail.com>
2019-07-17 14:11:06 +0200

    config

---
+++ config/Db.php
@@ -0,0 +1,8 @@
+<?php
+// Config serveur
+$host ='db5000093912.hosting-data.io';
+$dbname ='dbs88517';
+$user='dbu83591';
+$charset='utf8';
+$password='XXXXXXXX';
+$_ENV["DB"] = new PDO("mysql:host=$host;charset=$charset;dbname=$dbname", $user, $password);
